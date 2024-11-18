fc9d3a0067ce200355f8d81a0a7858b88be06e34
Amanda Tagoe <nayork77@gmail.com>
2024-11-18 11:13:36 +0000

    Modified consultation controller

--- src/validators/consultation.js
+++ src/validators/consultation.js
@@ -2,7 +2,6 @@ import Joi from "joi";
 
 // Validate consultation booking
 export const bookConsultationValidator = Joi.object({
-    userId: Joi.string().required(),
     consultationType: Joi.string().required(),
     date: Joi.string().required(),
     status: Joi.string()
